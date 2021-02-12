599c98947f5328dfaaf0b1cb9c18a4c6bc959f7f
Borek78 <70133614+Borek78@users.noreply.github.com>
2021-02-12 13:59:10 +0100

    Switch languges - finishes

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-const switchLanguage = functon;
+const switchLanguage = function () {
+  //Tlacitka
+  const czech = document.querySelector(".czech");
+  const english = document.querySelector(".english");
+
+  czech.addEventListener("click", function () {
+    console.log(czech);
+    english.classList.remove("active");
+    czech.classList.remove("active");
+    czech.classList.add("active");
+  });
+
+  english.addEventListener("click", function () {
+    console.log(czech);
+    czech.classList.remove("active");
+    english.classList.remove("active");
+    english.classList.add("active");
+  });
+};
+
+switchLanguage();
